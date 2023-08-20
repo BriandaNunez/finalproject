62ae388936981d9f167f254ff25ebc84f0e04f0a
BriandaNunez <140363057+BriandaNunez@users.noreply.github.com>
2023-08-19 22:48:54 -0700

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# main.py
+import logging
 import unittest
 import pandas as pd
 from fastapi import FastAPI
@@ -6,13 +6,35 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 
+# Configure the logger
+logging.basicConfig(filename='C:\\Users\\brianda.nunez\\Documents\\GitHub\\finalproject\\main.log', encoding='utf-8', level=logging.DEBUG)
+
+# Create a logger object
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+
+# Add a console handler and set its level to DEBUG
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# Create a formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add the formatter to the console handler
+ch.setFormatter(formatter)
+
+# Add the console handler to the logger
+logger.addHandler(ch)
+
 class TestDataExistence(unittest.TestCase):
     def test_data_existence(self):
         # Load training data
+        logger.debug('Loading training data')
         df_train = pd.read_csv(r'Project\finalproject_itesm_mlops\data\fraud_oracle.csv')
 
-        # Verify training data existance
+        # Verify training data existence
         self.assertFalse(df_train.empty, "No training data")
+        logger.info('Training data exists')
 
 class InputData(BaseModel):
     feature1: float
@@ -24,7 +46,7 @@ class OutputData(BaseModel):
 
 model = joblib.load(r'Project\finalproject_itesm_mlops\models\dtmodel.pkl')
 
-#add FastAPI to the project
+# Add FastAPI to the project
 
 app = FastAPI()
 
@@ -32,6 +54,7 @@ app = FastAPI()
 def train_model():
     # Implement the logic to train a new model
     # Return a response indicating the success or failure of the training process
+    logger.info('Training model')
     pass
 
 @app.post("/predict", response_model=OutputData)

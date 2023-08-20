5399517a2a035d7716d9f66e6d20a740bbc5e470
BriandaNunez <140363057+BriandaNunez@users.noreply.github.com>
2023-08-19 18:17:09 -0700

    commit

--- Project/finalproject_itesm_mlops/tests/testexistencedata.py
+++ Project/finalproject_itesm_mlops/tests/testexistencedata.py
@@ -1,13 +1,18 @@
-import unittest
+import logging
+import pytest
 import pandas as pd
 
-class TestDataExistence(unittest.TestCase):
-    def test_data_existence(self):
-        # Load training data
-        df_train = pd.read_csv(r'Project\finalproject_itesm_mlops\data\fraud_oracle.csv')
+# Set up logging configuration
+log_file = r'C:\Users\brianda.nunez\Documents\GitHub\finalproject\Project\finalproject_itesm_mlops\tests\testexistencedata.log'
+logging.basicConfig(filename=log_file, level=logging.INFO)
 
-        # Verify training data existance
-        self.assertFalse(df_train.empty, "No training data")
+def test_data_existence():
+    # Load training data
+    df_train = pd.read_csv(r'Project\finalproject_itesm_mlops\data\fraud_oracle.csv')
 
-if __name__ == '__main__':
-    unittest.main()
+    # Verify training data existence
+    if df_train.empty:
+        logging.error("No training data")
+        assert not df_train.empty, "No training data"
+    else:
+        logging.info("Training data loaded successfully")

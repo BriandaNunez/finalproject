cc2686129b58ced1569265d8f6b1f2d59a363478
BriandaNunez <140363057+BriandaNunez@users.noreply.github.com>
2023-08-19 18:21:55 -0700

    commit test

--- Project/finalproject_itesm_mlops/tests/transformations.py
+++ Project/finalproject_itesm_mlops/tests/transformations.py
@@ -1,7 +1,13 @@
+import pytest
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import logging
 
-#Test the custom label encoder from the fraud-detection notebook
+# Set up logging configuration
+log_file = r'C:\Users\brianda.nunez\Documents\GitHub\finalproject\Project\finalproject_itesm_mlops\tests\transformations.log'
+logging.basicConfig(filename=log_file, level=logging.INFO)
+
+# Test the custom label encoder from the fraud-detection notebook
 class CustomLabelEncoder:
     def __init__(self, col_ordering):
         self.col_ordering = col_ordering
@@ -25,30 +31,34 @@ class CustomLabelEncoder:
 
         return df
 
-# Create a sample DataFrame
-data = {
-    'col1': ['apple', 'orange', 'banana', 'orange'],
-    'col2': ['red', 'green', 'yellow', 'green']
-}
-df = pd.DataFrame(data)
+def test_label_encoder():
+    # Create a sample DataFrame
+    data = {
+        'col1': ['apple', 'orange', 'banana', 'orange'],
+        'col2': ['red', 'green', 'yellow', 'green']
+    }
+    df = pd.DataFrame(data)
 
-# Define the column ordering and corresponding mappings
-col_ordering = [
-    {'col': 'col1', 'mapping': {'apple': 1, 'banana': 2, 'orange': 3}},
-    {'col': 'col2', 'mapping': {'red': 4, 'green': 5, 'yellow': 6}}
-]
+    # Define the column ordering and corresponding mappings
+    col_ordering = [
+        {'col': 'col1', 'mapping': {'apple': 1, 'banana': 2, 'orange': 3}},
+        {'col': 'col2', 'mapping': {'red': 4, 'green': 5, 'yellow': 6}}
+    ]
 
-# Create an instance of CustomLabelEncoder
-encoder = CustomLabelEncoder(col_ordering)
+    # Create an instance of CustomLabelEncoder
+    encoder = CustomLabelEncoder(col_ordering)
 
-# Fit and transform the DataFrame using the CustomLabelEncoder
-transformed_df = encoder.fit_transform(df)
+    # Fit and transform the DataFrame using the CustomLabelEncoder
+    transformed_df = encoder.fit_transform(df)
 
-# Check if the transformation is valid
-is_valid = all(transformed_df[col].notna().all() for col in transformed_df.columns)
+    # Check if the transformation is valid
+    is_valid = all(transformed_df[col].notna().all() for col in transformed_df.columns)
 
-# Print the result
-if is_valid:
-    print("The transformation is valid.")
-else:
-    print("The transformation is not valid.")
+    # Log the result
+    if is_valid:
+        logging.info("The transformation is valid.")
+    else:
+        logging.error("The transformation is not valid.")
+
+    # Assert that the transformation is valid
+    assert is_valid
